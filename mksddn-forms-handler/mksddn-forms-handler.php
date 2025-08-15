f785da5ed2c7ed44838e6b68ff9de45e93ec992d
mksddn <maksim.anatolyevitch@gmail.com>
2025-08-15 23:15:40 +0300

    Remove unnecessary domain path and network settings from plugin header in mksddn-forms-handler.php

--- mksddn-forms-handler/mksddn-forms-handler.php
+++ mksddn-forms-handler/mksddn-forms-handler.php
@@ -11,8 +11,6 @@
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: mksddn-forms-handler
- * Domain Path: /languages
- * Network: false
  * 
  * @package MksDdnFormsHandler
  * @version 1.0.0
